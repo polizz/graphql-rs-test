3efe94b79b4e9becc06b5dac4365379f11f082d1
polizz <polizz@nowhere>
2024-04-16 18:22:00 -0500

    add tracing

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
+use async_graphql::extensions::Tracing as AsyncTracing;
 use async_graphql::{
     http::GraphiQLSource, Context, EmptyMutation, EmptySubscription, Object, Schema,
 };
 use async_graphql::{ComplexObject, Number, Parents, SimpleObject};
 use async_graphql_poem::*;
+// use poem::middleware::Tracing;
 use poem::{listener::TcpListener, web::Html, *};
 use sqlx::SqlitePool;
 use std::error::Error;
+use tracing_subscriber::EnvFilter;
 
 #[derive(sqlx::FromRow, Debug, SimpleObject)]
 #[graphql(complex)]
@@ -23,13 +26,11 @@ impl Customer {
     async fn customer_invoices<'c>(
         &self,
         ctx: &Context<'c>,
-        customer_id: i64,
     ) -> Result<Vec<Invoice>, anyhow::Error> {
         let conn = ctx.data_unchecked::<SqlitePool>();
 
         sqlx::query_as::<_, Invoice>("Select * from Invoice where CustomerId = $1")
-            // .bind(&self.CustomerId)
-            .bind(customer_id)
+            .bind(&self.CustomerId)
             .fetch_all(conn)
             .await
             .map_err(|e| e.into())
@@ -46,35 +47,32 @@ struct QueryRoot;
 
 #[Object]
 impl QueryRoot {
-    async fn customers<'ctx>(
+    async fn all_customers<'ctx>(
         &self,
         ctx: &Context<'ctx>,
-        customer_id: Option<i64>,
     ) -> Result<Vec<Customer>, anyhow::Error> {
-        // async fn load_all(&self) -> Vec<Self> {
         let conn = ctx.data_unchecked::<SqlitePool>();
-        // sqlx::query_as::<_, Customer>("Select LastName, FirstName from Customer limit 10;")
-        // let parent_id = ctx.path_node;
-        // dbg!(parent_id);
+
         sqlx::query_as::<_, Customer>("Select * from Customer;")
             .fetch_all(conn)
             .await
             .map_err(|e| e.into())
-
-        // if (ctx.look_ahead().field("invoices")) {
-        //     sqlx::query_as::<_, Customer>("Select * from Customer;")
-        //         .fetch_all(conn)
-        //         .await
-        //         .map_err(|e| e.into())
-        // } else {
-        //     sqlx::query_as::<_, Customer>("Select * from Customer;")
-        //         .fetch_all(conn)
-        //         .await
-        //         .map_err(|e| e.into())
-        // }
     }
 
-    async fn invoices<'ctx>(&self, ctx: &Context<'ctx>) -> Result<Vec<Invoice>, anyhow::Error> {
+    async fn customer_by_id<'ctx>(
+        &self,
+        ctx: &Context<'ctx>,
+        customer_id: Option<i64>,
+    ) -> Result<Customer, anyhow::Error> {
+        let conn = ctx.data_unchecked::<SqlitePool>();
+        sqlx::query_as::<_, Customer>("Select * from Customer where CustomerId = $1;")
+            .bind(customer_id)
+            .fetch_one(conn)
+            .await
+            .map_err(|e| e.into())
+    }
+
+    async fn all_invoices<'ctx>(&self, ctx: &Context<'ctx>) -> Result<Vec<Invoice>, anyhow::Error> {
         let conn = ctx.data_unchecked::<SqlitePool>();
         let parent_id = ctx.path_node;
         dbg!(parent_id);
@@ -96,9 +94,16 @@ async fn main() -> Result<(), Box<dyn Error>> {
     let conn = SqlitePool::connect("Chinook_Sqlite.sqlite").await?;
     // let conn = Sqlite::Connection::connect("sqlite::memory:").await?;
 
+    if let Ok(level) = std::env::var("RUST_LOG") {
+        tracing_subscriber::fmt()
+            .with_env_filter(EnvFilter::new(&format!("{level}")))
+            .init();
+    }
+
     // create the schema
     let schema = Schema::build(QueryRoot, EmptyMutation, EmptySubscription)
         .data(conn)
+        .extension(AsyncTracing)
         .finish();
 
     // start the http server
